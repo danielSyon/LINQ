4bf77c1d09a6554f38577a3ddcd35276d20b0b70
Daniel <daniel.villa@syon.com>
2023-04-03 13:56:19 +0200

    mejoras en el programa

--- Program.cs
+++ Program.cs
@@ -10,8 +10,19 @@ namespace pruebaLINQ
     {
         static void Main(string[] args)
         {
-           ControlEmpresasEmpleados jefe=new ControlEmpresasEmpleados();
-            jefe.getJefe();
+           ControlEmpresasEmpleados prog=new ControlEmpresasEmpleados();
+
+            Console.WriteLine("Introduce un id(1-2)");
+            
+            string entrada=Console.ReadLine();
+            try {
+                int entradaId= Convert.ToInt32(entrada);
+                prog.getEmpleadosEmpresa(entradaId);
+            }
+            catch(Exception) {
+                Console.WriteLine("No se ha podido introducir correctamente el dato");
+            }
+           
         }
     }
 
@@ -22,11 +33,17 @@ namespace pruebaLINQ
          listaEmpleados= new List<Empleado> ();
 
             listaEmpresas.Add(new Empresa { Id = 1, Nombre = "Syon" });
+
             listaEmpresas.Add(new Empresa { Id = 2, Nombre = "Panaderia Manolo" });
+
             listaEmpleados.Add(new Empleado { Cargo="Jefe",EmpresaId=1,Id=1,Nombre="Manolo",Salario=100000});
+
             listaEmpleados.Add(new Empleado { Cargo = "Empleado", EmpresaId = 1, Id = 2, Nombre = "Dani", Salario = 20000 });
+
             listaEmpleados.Add(new Empleado { Cargo = "Empleado", EmpresaId = 2, Id = 3, Nombre = "Pedro", Salario = 30000 });
+
             listaEmpleados.Add(new Empleado { Cargo = "Jefe", EmpresaId = 2, Id = 4, Nombre = "Luis", Salario = 900000 });
+
 
 
         }
@@ -34,7 +51,7 @@ namespace pruebaLINQ
         public void getJefe()
         {
 
-            IEnumerable<Empleado> jefes = from empleado in listaEmpleados where empleado.Cargo == "Empleado" select empleado;
+            IEnumerable<Empleado> jefes = from empleado in listaEmpleados where empleado.Cargo == "Jefe" select empleado;
 
             foreach (Empleado empleado1 in jefes)
             {
@@ -43,6 +60,28 @@ namespace pruebaLINQ
 
         }
 
+        public void getEmpleadosOrdenados()
+        {
+            IEnumerable<Empleado> empleados = from empleado in listaEmpleados orderby empleado.Nombre  select empleado;
+            foreach (Empleado empleado1 in empleados)
+            {
+                empleado1.getDatosEmpleado();
+            }
+
+        }
+
+        public void getEmpleadosEmpresa(int Id)
+        {
+            IEnumerable<Empleado> empleadosSyon = from empleado in listaEmpleados
+                                              join empresa in listaEmpresas on empleado.EmpresaId equals empresa.Id
+                                              where empresa.Id == Id
+                                              select empleado;
+            foreach (Empleado empleado1 in empleadosSyon)
+            {
+                empleado1.getDatosEmpleado();
+            }
+
+        }
         public List<Empresa> listaEmpresas;
         public List<Empleado> listaEmpleados;
 
